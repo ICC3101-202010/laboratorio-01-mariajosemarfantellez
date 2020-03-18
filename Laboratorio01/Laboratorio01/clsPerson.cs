5627a6bc215d40e5177347a35ce91bf846661982
mariajosemarfantellez <mjmarfan@miuandes.cl>
2020-03-18 20:59:34 -0300

    Ahora si

---
+++ Laboratorio01/Laboratorio01/clsPerson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio01
+{
+    class clsPerson
+    {
+        public string Name;
+        public string LastName;
+
+        public clsPerson(string X, string Y)
+        {
+            Name = X;
+            LastName = Y;
+        }
+    }
+}
