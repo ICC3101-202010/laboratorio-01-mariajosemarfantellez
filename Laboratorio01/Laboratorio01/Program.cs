443ab26079f2e9e04e5b222cf4444796498baa11
mariajosemarfantellez <mjmarfan@miuandes.cl>
2020-03-19 10:26:59 -0300

    Cambios Listos

--- Laboratorio01/Laboratorio01/Program.cs
+++ Laboratorio01/Laboratorio01/Program.cs
@@ -18,7 +18,7 @@ namespace Laboratorio01
 
             clsPerson Persona = new clsPerson("Bob", "Kunga");
             Console.WriteLine(Persona.Name + " " + Persona.LastName);
-            Console.ReadLine( );
+            Console.ReadLine();
         }
     }
 }
